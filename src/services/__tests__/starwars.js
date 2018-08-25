e8351c34aac78a56f6b6532a292fb3fef16e044e
Thibaut Cheymol <thibautc@theodo.fr>
2018-08-25 10:53:19 +0200

    ✨ Mock api call with Nock

--- src/services/__tests__/starwars.js
+++ src/services/__tests__/starwars.js
@@ -1,10 +1,21 @@
+import nock from 'nock';
 import { getStarwarsCharacter } from '../starwars';
+
+nock('https://swapi.co/api')
+  .get('/people/1')
+  .reply(
+    200,
+    {
+      name: 'Jango Fett',
+    },
+    { 'Access-Control-Allow-Origin': '*' },
+  );
 
 describe('starwars service', () => {
   describe('getStarwarsCharacter function', () => {
     it('should call starwars api with the given id', async () => {
       const luke = await getStarwarsCharacter(1);
-      expect(luke.name).toBe('Luke Skywalker');
+      expect(luke.name).toBe('Jango Fett');
     });
   });
 });
