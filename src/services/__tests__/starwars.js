70b2c1454b13fb0d67ddfe063d1c55c274a8d479
Thibaut Cheymol <thibautc@theodo.fr>
2018-08-25 11:40:40 +0200

    ✨ Add second test on getStarwarsCharacter

--- src/services/__tests__/starwars.js
+++ src/services/__tests__/starwars.js
@@ -7,7 +7,7 @@ jest.mock('../networking', () => ({
 
 describe('starwars service', () => {
   describe('getStarwarsCharacter function', () => {
-    it('should call starwars api with the given id', async () => {
+    it('should call starwars api to get the first person if we pas 1', async () => {
       const luke = await getStarwarsCharacter(1);
       expect(luke.name).toBe('Jabba the Hutt');
       expect(makeGetRequest).toHaveBeenCalledTimes(1);
@@ -15,5 +15,14 @@ describe('starwars service', () => {
         'https://swapi.co/api/people/1',
       );
     });
+  });
+
+  it('should call starwars api to get the second person if we pas 2', async () => {
+    const luke = await getStarwarsCharacter(2);
+    expect(luke.name).toBe('Jabba the Hutt');
+    expect(makeGetRequest).toHaveBeenCalledTimes(1);
+    expect(makeGetRequest).toHaveBeenCalledWith(
+      'https://swapi.co/api/people/2',
+    );
   });
 });
