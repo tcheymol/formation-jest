2d936e31011c18ca6ef6631c2bb475a266bcbd04
Thibaut Cheymol <thibautc@theodo.fr>
2018-08-25 09:31:33 +0200

    ✨ Add test without mock for getStarwardCharacter function

---
+++ src/services/__tests__/starwars.js
@@ -0,0 +1,10 @@
+import { getStarwarsCharacter } from '../starwars';
+
+describe('starwars service', () => {
+  describe('getStarwarsCharacter function', () => {
+    it('should call starwars api with the given id', async () => {
+      const luke = await getStarwarsCharacter(1);
+      expect(luke.name).toBe('Luke Skywalker');
+    });
+  });
+});
