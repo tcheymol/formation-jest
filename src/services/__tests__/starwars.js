1fb35f688a55d09caed6d4f7495db37230a30e12
Thibaut Cheymol <thibautc@theodo.fr>
2018-08-25 10:23:11 +0200

    💎 Add mock for makeGetRequest

--- src/services/__tests__/starwars.js
+++ src/services/__tests__/starwars.js
@@ -1,18 +1,15 @@
 import { getStarwarsCharacter } from '../starwars';
+import { makeGetRequest } from '../networking';
 
-global.fetch = jest.fn(() => {
-  return new Promise((resolve, reject) => {
-    resolve({
-      json: () => ({ name: 'Qui Gon Jinn' }),
-    });
-  });
-});
+jest.mock('../networking', () => ({
+  makeGetRequest: jest.fn(() => ({ name: 'Jabba the Hutt' })),
+}));
 
 describe('starwars service', () => {
   describe('getStarwarsCharacter function', () => {
     it('should call starwars api with the given id', async () => {
       const luke = await getStarwarsCharacter(1);
-      expect(luke.name).toBe('Qui Gon Jinn');
+      expect(luke.name).toBe('Jabba the Hutt');
     });
   });
 });
