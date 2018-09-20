cf1261820650e275a883c92dcdf30445c801c857
Thibaut Cheymol <thibautc@theodo.fr>
2018-09-20 23:31:57 +0200

    ✨ Locally mock superagent

--- src/services/__tests__/starwars.js
+++ src/services/__tests__/starwars.js
@@ -1,11 +1,18 @@
 // __tests__/starwars.js
 import { getStarwarsCharacter } from '../starwars';
 
+jest.mock('superagent', () => ({
+  get: () =>
+    new Promise((resolve, reject) => {
+      resolve({ body: { name: 'Wattoo Wattoo' } });
+    }),
+}));
+
 describe('starwars service', () => {
   describe('getStarwarsCharacter function', () => {
     it('should call starwars api with the given id', async () => {
       const luke = await getStarwarsCharacter(1);
-      expect(luke.name).toBe('Lando Kadrisian');
+      expect(luke.name).toBe('Wattoo Wattoo');
     });
   });
 });
